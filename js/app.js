ce4666a775b375cb49bba4973710a87256b2c80b
unknown <nicolasfleury@hotmail.com>
2022-10-13 01:49:02 -0300

    Implementación del carrito función adición

--- js/app.js
+++ js/app.js
@@ -2,83 +2,49 @@ window.setTimeout( function(){
 
 /* SCRIPT */
 
-var cantidad_total=0;
-let total;
+let add_carrito=[];
+let counter_carrito=[];
+let total=0;
 
-alert("Ingrese el nombre del producto que desea añadir al carrito, por ejemplo dragon o escopeta, en minúscula y sin tildes");
+carrito=document.getElementById("carrito_div")
 
-total=carrito();
+for(let i=0; i<8;i++){
 
-alert("Ha añadido un total de "+cantidad_total+" productos por un valor de $"+total);
+    add_carrito.push({boton: document.getElementById("add.item."+i), producto: document.getElementById("title.item."+i), precio: document.getElementById("price.item."+i)});
+    counter_carrito.push({sum: parseInt(0)});
 
-/* DECLARACIÓN DE FUNCIONES */
-
-function carrito(){
-
-    let parcial=0, subtotal=0;
-
-    do{
-
-        item=prompt("Si desea añadir un producto puede escribirlo, para salir ingrese 0");
-        parcial=valor(item);
-        subtotal=subtotal+parcial;
-        if(item!=0){
-            cantidad_total=cantidad_total+1;
-        }
-
-    }while(item!=0);
-
-    return subtotal;
 }
 
-function valor(producto){
+for(const element in add_carrito){
 
-    let precio=0;
-
-    switch(producto){
-
-        case "dragon":
-            precio=900;
-            break;
-
-        case "anana":
-            precio=500;
-            break;
-
-        case "groot":
-            precio=2600;
-            break;
-
-        case "pulpito":
-            precio=400;
-            break;
-
-        case "bananas":
-            precio=150;
-            break;
-
-        case "celular":
-            precio=2900;
-            break;
-            
-        case "computadora":
-            precio=5000;
-            break;
-            
-        case "escopeta":
-            precio=5700;
-            break;
-
-        case "0":
-            break;
-        
-        default:
-            alert("Producto inválido");
-            break;
-
+    add_carrito[element].boton.onclick = () => {
+        counter_carrito[element].sum += parseInt(1);
+        document.getElementById("empty_carrito").classList.add('hidenav');
+        document.getElementById("empty_carrito").classList.remove('shownav');
+        document.getElementById("carrito_total").classList.add('shownav');
+        document.getElementById("carrito_total").classList.remove('hidenav');
+        carrito_handler(add_carrito[element].producto.textContent, parseInt(add_carrito[element].precio.textContent), parseInt(counter_carrito[element].sum), element);
+        total += parseInt(add_carrito[element].precio.textContent);
+        document.getElementById("carrito_total_text").innerHTML = "Total $"+total;
     }
 
-    return precio;
+}
+
+function carrito_handler(producto,precio,sum,element){
+
+    let item_carro;
+
+    if(sum==1){
+        item_carro = document.createElement("nav");
+        item_carro.innerHTML = '<nav class="shownav" id=new_item_carrito><h4 id=new_item_text></h4></nav>';
+        carrito.append(item_carro);
+        document.getElementById("new_item_carrito").id = "item_carrito_"+element;
+        document.getElementById("new_item_text").id = "item_text_"+element;
+        document.getElementById("item_text_"+element).innerHTML = producto+" "+"$"+precio;
+    }else if(sum>1){
+        subtotal=sum*precio;
+        document.getElementById("item_text_"+element).innerHTML = producto+" "+"$"+subtotal;
+    }
 }
 
 }, 500);
